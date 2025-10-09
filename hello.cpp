97a630ef340b563955fd04a2569d902515b71c7a
varvara perova <st141846@student.spbu.ru>
2025-10-10 00:59:30 +0300

    hello_world

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+#include "hello.h"
+
+#include <iostream>
+#include <string>
+
+void myproject::sayHello(const std::string& name) {
+	std::string name
+	std::cout << "Hello, world!" << std::endl;
+	std::getline(std::cin, name);
+	std::cout << "Hello, " << name << "!" <<  std::endl;
+}
---
+++ hello.h
@@ -0,0 +1,10 @@
+#ifndef HELLO_H
+#define HELLO_H
+
+#include <string>
+
+namespace myproject {
+	void sayHello(const std::string& name);
+}
+
+#endif
