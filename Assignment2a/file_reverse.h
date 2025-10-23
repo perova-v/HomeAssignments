94e973b533750bc008611543660d24c5024e020a
perova-v <st141846@student.spbu.ru>
2025-10-24 01:30:06 +0300

    1p

---
+++ Assignment2a/file_reverse.h
@@ -0,0 +1,14 @@
+/*
+ * Perova Varvara
+ * st141846@student.sbpu.ru
+ * My project number 2a
+ */
+
+#ifndef FILE_REVERSE.H
+#define FILE_REVERSE.H
+
+#include <string>
+
+void reverseFile(const std::string& inputFile);
+
+#endif
