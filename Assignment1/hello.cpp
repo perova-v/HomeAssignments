d6b1ce850bf468a0f6281bb4729d3b32a4b0818a
perova-v <st141846@student.spbu.ru>
2025-11-07 14:42:55 +0300

    Added a loop

--- Assignment1/hello.cpp
+++ Assignment1/hello.cpp
@@ -9,9 +9,19 @@
 #include <iostream>
 #include <string>
 
+using namespace std;
+
 void myproject::sayHello() {
-        std::string name;
-        std::cout << "Hello, world!" << std::endl;
-        std::getline(std::cin, name);
-        std::cout << "Hello, " << name << "!" <<  std::endl;
+        string name;
+        cout << "Hello, world!" << endl << endl;
+
+	cout << "Enter a name (or \"exit\" to end the programm): " << endl;
+	getline(cin, name);
+
+	while (name != "exit")
+	{
+        cout << "Hello, " << name << "!" <<  endl << endl;
+	cout << "Enter a name (or \"exit\" to end the programm): " << endl;
+	getline(cin, name);
+	}
 }
